c4bfa212a4810037ce26b03e2d2b9349079732f0
A M D <52590027+mas-alone@users.noreply.github.com>
2023-10-10 17:53:33 +0800

    Update __init__.py

--- nonebot_plugin_acc_calculate/__init__.py
+++ nonebot_plugin_acc_calculate/__init__.py
@@ -1,28 +1,43 @@
-from nonebot.adapters import Message
-from nonebot.params import ArgStr,CommandArg
-from nonebot.matcher import Matcher
+from nonebot.adapters.onebot.v11 import Event, MessageSegment, Bot
+from nonebot.rule import to_me
 from nonebot import on_command
 from .data_source import calculate_acc
+import time
+from nonebot import get_driver
+command_start = get_driver().config.command_start
 
-malody = on_command('/acc')
+help_msg = '''
+指令说明:-acc [t] [单曲acc值]
 
+t为查询的段位(*dan|*danv2|rf*)[*换成对应的数字或字母] 下面是例子:
+-acc 2dan 99.26-98.63-97.32-96.11
+'''
+help = on_command("acc help", aliases={'acc help', })
+@help.handle()
+async def _(bot: Bot, event: Event):
+    await help.finish(help_msg, at_sender=True)
+
+malody = on_command('acc')
 @malody.handle()
-async def _handle(matcher: Matcher,msg: Message = CommandArg()):
-    msg = msg.extract_plain_text().split(" ")
-    if len(msg) == 2:
-        matcher.set_arg("t",msg[0])
-        matcher.set_arg("accs",msg[1])
-
-@malody.got("t", prompt = '请输入要查询的段位(regular*|regular*v2|ex*|ex*v2|reform*)[*换成对应的数字或字母]')
-@malody.got('accs', prompt = '请按顺序输入acc如：99.26-98.63-97.32-96.11')
-async def _got(t = ArgStr("t"),accs = ArgStr("accs")):
-    accs = accs.strip().split("-")
-    try:
-        result = calculate_acc(accs[0],accs[1],accs[2],accs[3],t)
-        acc1 = str(result[0])[:5]
-        acc2 = str(result[1])[:5]
-        acc3 = str(result[2])[:5]
-        acc4 = str(result[3])[:5]
-    except:
-        await malody.finish("失败，输入参数有误或不存在",at_sender=True)
-    await malody.finish("\n您的第一首acc是:{}%\n您的第二首acc是:{}%\n您的第三首acc是:{}%\n您的第四首acc是:{}%".format(acc1,acc2,acc3,acc4),at_sender=True)
+async def _(bot: Bot, event: Event):
+    msg = str(event.get_message())
+    msg = msg.strip()
+    if msg[0] in command_start:
+        msg = msg[1:]
+    msg = msg.replace("acc", "").strip().split(" ")
+    t = msg[0]
+    accs = msg[1].split("-")
+    if any(float(acc) > 100 for acc in accs):
+        await malody.finish("acc哪有大于100的,搞事是吧..!", at_sender=True)
+    else:
+        try:
+            result = calculate_acc(accs[0], accs[1], accs[2], accs[3], t)
+            acc1 = str(result[0])[:5]
+            acc2 = str(result[1])[:5]
+            acc3 = str(result[2])[:5]
+            acc4 = str(result[3])[:5]
+        except Exception as e:
+            await malody.finish("失败，输入参数有误或不存在", at_sender=True)
+        await malody.finish("\n您的第一首acc是:{}%\n您的第二首acc是:{}%\n您的第三首acc是:{}%\n您的第四首acc是:{}%".format(acc1, acc2,
+                                                                                                          acc3, acc4),
+                        at_sender=True)
